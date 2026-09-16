d92fdc13f8edd44c5ba402ade40caea3c0f3b641
Aravind <sathishkumarhollak@gmail.com>
2026-09-16 19:49:13 +0530

    Create STRT.txts for tracker system
    Add functionality to create a tracker system file.

--- src/application/app_init.c
+++ src/application/app_init.c
@@ -6,5 +6,8 @@ char instruction2[]="mkdir APP_DATA_HYLINK/Downloads";
 system(instruction2);
 char instruction3[]="mkdir APP_DATA_HYLINK/Data";
 system(instruction3);
+FILE *fp=fopen("STRT.txts","w");
+fprintf(fp,"NULL but empty a tracker system");
+fclose(fp);
 return 0;
 }
