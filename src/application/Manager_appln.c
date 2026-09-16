1cfc852d9ae3d734780ea1f5c0786ea0252efe74
Aravind <sathishkumarhollak@gmail.com>
2026-09-16 20:00:12 +0530

    Update Manager_appln.c

--- src/application/Manager_appln.c
+++ src/application/Manager_appln.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include"logs.h"
+int check(){
+FILE *fp=NULL;
+fp=fopen("APP_DATA_HYLINK/Data/STRT.txts","r");
+if(fp==NULL){
+return 1;
+}
+fclose(fp);
+fp=NULL;
+fp=fopen("APP_DATA_HYLINK/Downloads/STRT.txts","r");
+if(fp==NULL){
+return  1;
+}
+return 0;
+}
+int main(int n,char **s){
+if(check()){
+system("./app_init");
+}
 
+return 1;
+}
